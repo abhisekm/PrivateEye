93b6d185ebe07efeb9c7c2a966508170881e5e9d
abhisekm <abhisekmajumder07@gmail.com>
2015-12-07 20:57:33 +0530

    Final Draft

--- app/src/main/java/com/abhisek/privateeye/MyAccessibilityService.java
+++ app/src/main/java/com/abhisek/privateeye/MyAccessibilityService.java
@@ -1,22 +1,39 @@
 package com.abhisek.privateeye;
 
 import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
 
 public class MyAccessibilityService extends AccessibilityService {
 
+    static final String TAG = "PrivateEye";
+
     public MyAccessibilityService() {
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED &&
-                event.getSource()!= null)
-        {
-            Toast.makeText(this, getEventDescription(event.getSource()),Toast.LENGTH_LONG).show();
+        if (event.getPackageName().toString().contains("com.google.android.googlequicksearchbox") ||
+                event.getPackageName().toString().contains("com.android.systemui"))
+            return;
+
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED &&
+                event.getSource() != null) {
+            String msg = getEventDescription(event.getSource());
+            if (msg.length() > 0)
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         }
+
+        Log.v(TAG, String.format(
+                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [source] %s",
+                AccessibilityEvent.eventTypeToString(event.getEventType()), event.getClassName(),
+                event.getPackageName(), event.getEventTime(),
+                event.getSource() == null ? "" : event.getSource().getClassName()));
+
+
     }
 
     @Override
@@ -38,7 +55,7 @@ public class MyAccessibilityService extends AccessibilityService {
             sb.append('\n');
         }
 
-        for(int i=0;i<node.getChildCount();i++){
+        for (int i = 0; i < node.getChildCount(); i++) {
             sb.append(getEventDescription(node.getChild(i)));
         }
 
